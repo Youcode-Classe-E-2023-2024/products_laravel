cdb4edef38afc8abe6268e0b1e21baea952ccb44
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-29 16:40:47 +0100

    add read

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\product;
 class ProductController extends Controller
 {
     //
+    function index() {
+        $products = product::All();
+        return view("products", compact("products"));
+    }
 }
