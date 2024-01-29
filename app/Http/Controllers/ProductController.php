a3d99c90cf8efb88835824eca957c6c2fd086e50
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-29 16:18:40 +0100

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('product.show');
+    }
+
+    public function create()
+    {
+        return view('product.create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/products', [productController::class , 'index']);
+Route::get('/product/create', [productController::class , 'create']);
 
 Route::resource('products', ProductController::class);
